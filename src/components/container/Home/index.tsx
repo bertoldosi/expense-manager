44f1a9d8172f5c796728409eff13dcc27d8bbadd
bertoldosi <bertoldosi>
2023-09-12 08:14:44 -0300

    refactor: Refatorando componente principal

--- src/components/container/Home/index.tsx
+++ src/components/container/Home/index.tsx
@@ -1,15 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import Cookies from "universal-cookie";
+import moment from "moment";
 
 import { Institution } from "@containers/Home/Institution";
 import WithoutInstitution from "@containers/Home/Institution/WithoutInstitution";
 
 import { Scontainer } from "./styles";
 import InstitutionMenuFilter from "./InstitutionMenuFilter";
-import instances from "@lib/axios-instance-internal";
-import { ExpenseType, UserType } from "@interfaces/*";
 import { useSession } from "next-auth/react";
-import moment from "moment";
 import { userContext, userContextType } from "@context/userContext";
 
 const keyCookies = "expense-manager";
@@ -18,13 +16,7 @@ function Home() {
   const cookies = new Cookies();
 
   const { data: session } = useSession();
-  const {
-    setUser,
-    setExpense,
-    setInstitution,
-    expense,
-    setSelectedInstitution,
-  } = useContext(userContext) as userContextType;
+  const { expense } = useContext(userContext) as userContextType;
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [valueYear, setValueYear] = useState<number>(() => {
@@ -40,166 +32,185 @@ function Home() {
     return month;
   });
 
-  async function setDateFilter() {
-    const cookieValues = cookies.get("expense-manager");
+  function persistDate(date: string) {
+    const [_day, month, year] = date.split("/");
 
-    if (cookieValues?.filter?.institutions?.createAt) {
-      const fullDateCookies = cookieValues?.filter?.institutions?.createAt;
-
-      const [_day, month, year] = fullDateCookies.split("/");
-
-      setValueYear(Number(year));
-      setValueMonth(month);
-    } else {
-      const date = moment().format("DD/MM/YYYY");
-      const [_day, month, year] = date.split("/");
-
-      const newCookies = {
-        ...cookieValues,
-        filter: {
-          ...cookieValues?.filter,
-          institutions: {
-            createAt: `01/${month}/${year}`,
-          },
-        },
-      };
-
-      cookies.set("expense-manager", newCookies);
-
-      setValueYear(Number(year));
-      setValueMonth(month);
-    }
+    setValueYear(Number(year));
+    setValueMonth(month);
   }
 
-  function persistCookies(expense: ExpenseType) {
+  function getDateNow() {
     const cookieValues = cookies.get(keyCookies);
-    setExpense(expense);
+    const date = moment().format("DD/MM/YYYY");
+    const [_day, month, year] = date.split("/");
+    const dateSelected = `01/${month}/${year}`;
 
-    /* sempre persistir o expense do user*/
-    cookies.set(keyCookies, {
+    const newCookieValues = {
       ...cookieValues,
       filter: {
-        ...cookieValues.filter,
-        expense: {
-          id: expense.id,
-          name: expense.name,
-        },
+        ...cookieValues?.filter,
+        dateSelected,
       },
-    });
+    };
 
-    const institutionCookies = cookieValues?.filter?.institution;
-    const firstInstitutionExpense = expense?.institutions?.length
-      ? expense.institutions[0]
-      : null;
-
-    /* se existir institution no cookies*/
-    if (institutionCookies) {
-      //verificar se dentro da instituições, existe essa salva no cookies
-
-      const existInstitutionInInstitutions = expense?.institutions?.length
-        ? expense.institutions.find(
-            (findInstitution) =>
-              findInstitution.name === institutionCookies.name
-          )
-        : null;
-
-      if (existInstitutionInInstitutions) {
-        setSelectedInstitution(existInstitutionInInstitutions);
-        setInstitution(existInstitutionInInstitutions);
-      } else {
-        setSelectedInstitution(null);
-        setInstitution(null);
-
-        cookies.set(keyCookies, {
-          ...cookieValues,
-          filter: {
-            ...cookieValues.filter,
-            institution: null,
-          },
-        });
-      }
-    } else {
-      setSelectedInstitution(firstInstitutionExpense);
-      setInstitution(firstInstitutionExpense);
-
-      cookies.set(keyCookies, {
-        ...cookieValues,
-        filter: {
-          ...cookieValues.filter,
-          institution: firstInstitutionExpense
-            ? {
-                id: firstInstitutionExpense?.id,
-                name: firstInstitutionExpense?.name,
-              }
-            : null,
-        },
-      });
-    }
+    setValueYear(Number(year));
+    setValueMonth(month);
+    cookies.set(keyCookies, newCookieValues);
   }
 
-  async function createExpense(user: UserType) {
-    await instances
-      .post("api/expense", {
-        name: "default",
-        userEmail: user.email,
-      })
-      .then(({ data: expense }) => {
-        return persistCookies(expense);
-      })
-      .catch((error) => {
-        console.log("error ao criar expense", error);
-      });
-  }
-
-  async function fecthUser(email: string) {
+  function initializationDate() {
     const cookieValues = cookies.get(keyCookies);
+    const createAt = cookieValues?.filter?.dateSelected;
 
-    await instances
-      .get("api/user", {
-        params: {
-          email,
-        },
-      })
-      .then(async ({ data: user }) => {
-        //criar novo gasto caso o usuario não possua
-        if (!user?.expense) {
-          return await createExpense(user);
-        }
+    // caso exista uma data já selecionada, salvamos localmente
+    if (createAt) {
+      return persistDate(createAt);
+    }
 
-        await instances
-          .get("api/institution", {
-            params: {
-              createAt: cookieValues?.filter?.institutions?.createAt,
-              expenseId: user?.expense?.id,
-            },
-          })
-          .then(({ data: institutions }) => {
-            const expenseGet = {
-              ...user.expense,
-              institutions,
-            };
-
-            setUser({ ...user, expense: expenseGet });
-            setExpense(expenseGet);
-            persistCookies(expenseGet);
-          });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    setIsLoading(false);
+    // caso não exista uma data já selecionada, pegamos a data atual e salvamos localmente
+    getDateNow();
   }
+
+  function getUser(email: string) {
+    console.log(email);
+  }
+
+  // function persistCookies(expense: ExpenseType) {
+  //   const cookieValues = cookies.get(keyCookies);
+  //   setExpense(expense);
+
+  //   /* sempre persistir o expense do user*/
+  //   cookies.set(keyCookies, {
+  //     ...cookieValues,
+  //     filter: {
+  //       ...cookieValues.filter,
+  //       expense: {
+  //         id: expense.id,
+  //         name: expense.name,
+  //       },
+  //     },
+  //   });
+
+  //   const institutionCookies = cookieValues?.filter?.institution;
+  //   const firstInstitutionExpense = expense?.institutions?.length
+  //     ? expense.institutions[0]
+  //     : null;
+
+  //   /* se existir institution no cookies*/
+  //   if (institutionCookies) {
+  //     //verificar se dentro da instituições, existe essa salva no cookies
+
+  //     const existInstitutionInInstitutions = expense?.institutions?.length
+  //       ? expense.institutions.find(
+  //           (findInstitution) =>
+  //             findInstitution.name === institutionCookies.name
+  //         )
+  //       : null;
+
+  //     if (existInstitutionInInstitutions) {
+  //       setSelectedInstitution(existInstitutionInInstitutions);
+  //       setInstitution(existInstitutionInInstitutions);
+  //     } else {
+  //       setSelectedInstitution(null);
+  //       setInstitution(null);
+
+  //       cookies.set(keyCookies, {
+  //         ...cookieValues,
+  //         filter: {
+  //           ...cookieValues.filter,
+  //           institution: null,
+  //         },
+  //       });
+  //     }
+  //   } else {
+  //     setSelectedInstitution(firstInstitutionExpense);
+  //     setInstitution(firstInstitutionExpense);
+
+  //     cookies.set(keyCookies, {
+  //       ...cookieValues,
+  //       filter: {
+  //         ...cookieValues.filter,
+  //         institution: firstInstitutionExpense
+  //           ? {
+  //               id: firstInstitutionExpense?.id,
+  //               name: firstInstitutionExpense?.name,
+  //             }
+  //           : null,
+  //       },
+  //     });
+  //   }
+  // }
+
+  // async function createExpense(user: UserType) {
+  //   await instances
+  //     .post("api/expense", {
+  //       name: "default",
+  //       userEmail: user.email,
+  //     })
+  //     .then(({ data: expense }) => {
+  //       return persistCookies(expense);
+  //     })
+  //     .catch((error) => {
+  //       console.log("error ao criar expense", error);
+  //     });
+  // }
+
+  // async function fecthUser(email: string) {
+  //   const cookieValues = cookies.get(keyCookies);
+
+  //   await instances
+  //     .get("api/user", {
+  //       params: {
+  //         email,
+  //       },
+  //     })
+  //     .then(async ({ data: user }) => {
+  //       //criar novo gasto caso o usuario não possua
+  //       if (!user?.expense) {
+  //         return await createExpense(user);
+  //       }
+
+  //       await instances
+  //         .get("api/institution", {
+  //           params: {
+  //             createAt: cookieValues?.filter?.institutions?.createAt,
+  //             expenseId: user?.expense?.id,
+  //           },
+  //         })
+  //         .then(({ data: institutions }) => {
+  //           const expenseGet = {
+  //             ...user.expense,
+  //             institutions,
+  //           };
+
+  //           setUser({ ...user, expense: expenseGet });
+  //           setExpense(expenseGet);
+  //           persistCookies(expenseGet);
+  //         });
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+
+  //   setIsLoading(false);
+  // }
 
   useEffect(() => {
-    setDateFilter();
+    initializationDate();
   }, []);
 
   useEffect(() => {
-    if (session?.user?.email) {
-      fecthUser(session.user.email);
+    const userEmail = session?.user?.email;
+    if (userEmail) {
+      getUser(userEmail);
     }
   }, [session]);
+
+  // useEffect(() => {
+  //   if (session?.user?.email) {
+  //     fecthUser(session.user.email);
+  //   }
+  // }, [session]);
 
   return (
     <Scontainer>
