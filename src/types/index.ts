25244f15a3bb1522c4e69dbd6f067e859ec38a2f
bertoldosi <bertoldosi>
2023-09-19 10:14:42 -0300

    refactor: Ajustando types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,59 +1,26 @@
-export type UserType = {
-  id?: string;
-  email: string;
-  name: string;
-  expenses: ExpenseType[];
-};
-
-interface TotalPerDateType {
-  date: string;
-  total: number;
-}
-
-interface CategoryTotalPerDateType {
-  date: string;
-  categoryTotals: CategoryType[];
-}
-
-export type ExpenseType = {
-  id: string;
-  name: string;
-  totalPerDate: TotalPerDateType[];
-  categoryTotalPerDate: CategoryTotalPerDateType[];
-  institutions?: InstitutionType[];
-};
-
-export type InstitutionType = {
-  id: string;
-  name: string;
-  amount?: string | null;
-  totalAmount?: number;
-  categoryTotals?: CategoryType[];
-  shoppings?: ShoppingType[] | null;
-  createAt: string;
-};
-
 export type ShoppingType = {
   id: string;
   description: string;
   amount: string;
   category: string;
   paymentStatus: string;
-  selected?: boolean;
-  institutionId?: string;
 };
 
-export type CategoryType = {
-  category: string;
-  total: number;
+export type InstitutionType = {
+  id: string;
+  name: string;
+  createAt: string;
+  shoppings: ShoppingType[] | null;
 };
 
-export type CategoryTotalsMonthType = {
-  date: string;
-  categoryTotals: CategoryType[];
+export type ExpenseType = {
+  id: string;
+  name: string;
+  institutions: InstitutionType[] | null;
 };
 
-export type TotalAmountType = {
-  date: string;
-  total: number;
+export type UserType = {
+  email: string;
+  name: string;
+  expense: ExpenseType;
 };
